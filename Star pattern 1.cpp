4d109fac48ff314efd7bb79aaaca4d0f6746dcc4
theadityabasak <orandom46@gmail.com>
2024-08-10 20:06:03 +0530

    Add files via upload
    Star pattern using nested loops

---
+++ Star pattern 1.cpp
@@ -0,0 +1,21 @@
+//Aditya Basak
+//23070123007
+//Entc=A
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=0;i<5;i++){
+        for(j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+/*
+*
+**
+***
+****
+*****
+*/
